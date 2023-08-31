02baf78e70fcd8ed4debb88fdb86b43bbb9edc3b
Muhammad-Sowrov <muhammadsowrov53@gmail.com>
2023-08-31 12:26:13 +0600

    spinner done

--- script/script.js
+++ script/script.js
@@ -17,8 +17,8 @@
 
 
 
-const loadPhone = async()=>{
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async(searchText)=>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
     displayPhones(phones);
@@ -28,10 +28,24 @@ const loadPhone = async()=>{
 const displayPhones = phones => {
     // console.log(phones);
     const phoneContainer = document.getElementById('phone-container');
+    // CLEAR CONTAINER
+    phoneContainer.textContent = '',
+    // // show all
+
+    showAll = document.getElementById('show-all-container');
+    if(phones.length > 10){
+        showAll.classList.remove('hidden');
+    }
+    else{
+        showAll.classList.add('hidden');
+    }
+    // only show 10 phone
+    // console.log(phones.length);
+    phones = phones.slice(0,10);
     phones.forEach(phone =>{
         console.log(phone);
         const phoneCard = document.createElement('div');
-        phoneCard.classList = 'card w-96 bg-slate-400 shadow-xl ';
+        phoneCard.classList = 'card bg-slate-400 shadow-xl ';
         phoneCard.innerHTML = `
         <figure class="px-10 pt-10">
                       <img src="${phone.image}" alt="Shoes" class="rounded-xl" />
@@ -45,8 +59,27 @@ const displayPhones = phones => {
         </div>
         `
         phoneContainer.appendChild(phoneCard);
-    })
+    });
+    loadSpin(false);
 }
 
+// search
+const handleSearch = () =>{
+    loadSpin(true)
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhone(searchText);
+    
+}
 
-loadPhone();
+const loadSpin = (isLoading) =>{
+    const load = document.getElementById('spinner');
+    // load.classList.remove('hidden')
+    if(isLoading){
+        load.classList.remove('hidden');
+    }
+    else{
+        load.classList.add('hidden')
+    }
+}
+// loadPhone();
